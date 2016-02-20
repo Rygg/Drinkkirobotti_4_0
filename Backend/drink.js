7818ff1411611036d0a7679474ad296e028e02d0
Ville Lepistö <ville.lepisto@student.tut.fi>
2016-02-21 00:29:59 +0200

    Muutettu tyyli ES6 Javascriptiin, selkeämpää, mukavampaa ja parempaa.

--- Backend/drink.js
+++ Backend/drink.js
@@ -1,69 +1,79 @@
 // Source code for the drink class.
 
+"use strict"; // Force to strict mode for Class and Let usage.
 
 // Data structure for portion
-var Portion = function(bottlename, amount) {
-    this.bottleName = bottlename;
-    this.amount = amount;
+class Portion {
+    constructor(bottlename, amount) {
+        this.bottleName = bottlename;
+        this.amount = amount;
+    }
 };
 
-// Datastructure for a drink.
-var Drink = function(name) {
-    this.name = name;
-    this.available = false;
-    this.recipe = [];
+// Datastructure for the drink class:
+class Drink {
+    constructor(name) {
+        this.name = name;
+        this.available = false;
+        this.recipe = [];
+    }
+
+    // Function for adding a portion into the recipe-array. Incase of duplicates, changes the amount of the drink.
+    addPortion(pname, pamount) {
+        // Check for invalid amount:
+        if (arguments.length < 1 || typeof(pamount) != "number") {
+            console.log('Invalid amount when adding ' + pname + ' to recipe.')
+            return;
+        }
+        // Check for duplicates:
+        for (let i = 0; i < this.recipe.length; i++) {
+            if (this.recipe[i].bottleName == pname) {
+                this.recipe[i].amount = pamount;
+                console.log(pname + ' already found in the recipe. Changed the amount to ' + pamount + 'cl.');
+                return;
+            }
+        }
+        // Add a new portion:
+        let newportion = new Portion(pname, pamount)
+        this.recipe.push(newportion);
+        console.log('Added ' + pamount + 'cl of ' + pname + ' to the recipe.')
+    }
+
+    // Function for removing the portion of the wanted name from the recipe-array.
+    removePortion(pname) {
+        // Search and remove:
+        for(let i = 0; i < this.recipe.length; i++) {
+            if (this.recipe[i].bottleName == pname) {
+                this.recipe.splice(i, 1);
+                console.log('Removed ' + pname + ' from the recipe.')
+                return;
+            }
+        }
+        // Not found:
+        console.log(pname + ' not found in the recipe.')
+    }
+
+    // Function which checks if the drink can be mixed with current bottles:
+    checkAvailability() {
+        //Todo:
+        this.available = true;
+    }
+
+    // Function, which turns the portions in the recipe into JSON-strings and returns it in an array.
+    recipetoJSON() {
+        if (this.recipe.length < 1) {
+            console.log('No recipe.')
+            return;
+        }
+        let portions = [];
+        let string;
+        for (let i = 0; i < this.recipe.length; i++) {
+            string = JSON.stringify(this.recipe[i]);
+            portions.push(string);
+        }
+        return portions;
+    }
+
 };
 
-// Function for adding a portion into the recipe-array. Incase of duplicates, changes the amount.
-Drink.prototype.addPortion = function (pname, pamount) {
-    if (arguments.length < 1 || typeof(pamount) != "number") {
-        console.log('Invalid amount when adding ' + pname + ' to recipe.')
-        return;
-    }
-    for (i = 0; i < this.recipe.length; i++) {
-        if (this.recipe[i].bottleName == pname) {
-            this.recipe[i].amount = pamount;
-            console.log(pname + ' already found in the recipe. Changed the amount to ' + pamount + 'cl.');
-            return;
-        }
-    }
-    newportion = new Portion(pname, pamount)
-    this.recipe.push(newportion);
-    console.log('Added ' + pamount + 'cl of ' + pname + ' to the recipe.')
-}
-
-// Function for removing the portion of the wanted name from the recipe-array.
-Drink.prototype.removePortion = function (pname) {
-    // Search and remove:
-    for(i = 0; i < this.recipe.length; i++) {
-        if (this.recipe[i].bottleName == pname) {
-            this.recipe.splice(i, 1);
-            console.log('Removed ' + pname + ' from the recipe.')
-            return;
-        }
-    }
-    // Not found:
-    console.log(pname + ' not found in the recipe.')
-}
-
-// Function which checks if there are ingredients for preparing the drink.
-Drink.prototype.checkAvailability = function () {
-    //Todo:
-    this.available = true;
-}
-
-// Function, which turns the portions in the recipe into JSON-strings and returns it in an array.
-Drink.prototype.recipetoJSON = function () {
-    if (this.recipe.length < 1) {
-        console.log('No recipe.')
-        return;
-    }
-    var portions = [];
-    var string;
-    for (i = 0; i < this.recipe.length; i++) {
-        string = JSON.stringify(this.recipe[i]);
-        portions.push(string);
-    }
-    return portions;
-}
 module.exports = Drink
--- Backend/drinkdb.js
+++ Backend/drinkdb.js
@@ -1,41 +1,50 @@
 // Source code for drinkdatabase functions
+"use strict";
 
 // Import Drink-class
-var Drink = require("./drink.js");
+let Drink = require("./drink.js");
 
 // The definition for the class
-var DrinkDB = function () {
-    this.drinks = [];
+class DrinkDB {
+    constructor(){
+        this.drinks = [];
+    }
+    
+    // Function which adds a drink to the drinks array. Assumes a string and an array of portions as parameters
+    addDrink(name, recipe) {
+        console.log('Adding drink ' + name + ' to the database.');
+
+        // Create the drink
+        let beverage = new Drink(name);
+        // Copy the recipe from the parameter
+        for (let i = 0; i < recipe.length; i++) {
+            beverage.addPortion(recipe[i].bottleName, recipe[i].amount);
+        }
+        // Add to the container
+        this.drinks.push(beverage);
+    
+        console.log('Succesfully added drink ' + name + ' to the database.');
+    }
+    
+    // The function that reads the drink database from a separate file.
+    initialize() {
+        return;
+        //Todo
+    }
+    
+    // The function which writes the drink database into a separate file.
+    export() {
+        //todo
+        let drinklist = JSON.stringify(this.drinks[0]);
+        console.log(drinklist);
+    }
 };
 
-
-// Function which adds a drink to the drinks array. Assumes a string and an array of portions as parameters
-DrinkDB.prototype.addDrink = function (name, recipe) {
-    console.log('Adding drink ' + name + ' to the database.');
-    beverage = new Drink(name);
-    for (i = 0; i < recipe.length; i++) {
-        beverage.addPortion(recipe[i].bottleName, recipe[i].amount);
-    }
-    console.log('Succesfully added drink ' + name + 'to the database.');
-}
-
-// The function which reads an existing drinkdatabase from a file.
-DrinkDB.prototype.initialize = function () {
-    // Todo
-}
-
-
-
-// The function which writes the drink database into a separate file.
-DrinkDB.prototype.export = function () {
-    var drinklist = JSON.stringify(this.drinks);
-    console.log(drinklist);
-}
 
 
 
 console.log('Testing intensifies:')
-var GT = new Drink('GT');
+let GT = new Drink('GT');
 GT.addPortion('Gin', 4);
 GT.addPortion('Tonic', 6);
 GT.addPortion('Kalja');
@@ -49,15 +58,16 @@ GT.removePortion('Tonic');
 console.log(GT.recipe);
 
 // For testing, cp:d from the file
-var Portion = function (bottlename, amount) {
+let Portion = function (bottlename, amount) {
     this.bottleName = bottlename;
     this.amount = amount;
 };
 
-var DB = new DrinkDB();
-var resepti = [];
+let DB = new DrinkDB();
+let resepti = [];
 resepti.push(new Portion("Jallu", 4));
 resepti.push(new Portion("Kahvi", 4));
+console.log(resepti);
 
-DrinkDB.addDrink('Jallukahvi', resepti);
-DrinkDB.export;
+DB.addDrink('Jallukahvi', resepti);
+DB.export;
