75d9a67e88b40efd610afbe35dfc417eb183e56d
Ville <ville.lepisto@student.tut.fi>
2016-03-16 17:47:09 +0200

    Kirjoitettu jotain funktioita nopeasti tiedonhakukoulutuksessa.

--- Backend/db.js
+++ Backend/db.js
@@ -18,8 +18,7 @@ class Database {
     // QueueObject-JSON for the main program to use.
     // returns false if the drink is not available in the database, though assumes it is in there.
     // Implementation for ID parameter is missing, assuming parameter.
-    reserveDrink(drinkName, id) {
-        
+    reserveDrink(drinkName, id) {        
         // Search the drink from the database.
         let usedDrink;
         for (let i = 0; i < this.drinkDB.drinks.length; i++) {
@@ -44,7 +43,7 @@ class Database {
         }
         
         return {
-            Drink: usedDrink,
+            drink: usedDrink,
             locations: usedLocations,
             ID: id
         };
@@ -53,27 +52,52 @@ class Database {
     // Function for canceling a drink from the queue. Returns the reservedShelf back to the original value before the order.
     // Returns true for a succesful operation and false for failure.
     cancelDrink(qObject) {
-        return;
+        // Go through the recipe of the drink and add the correct amounts back to the bottles:
+        // try-catch incase something weird has happened with the indeces.
+        try {
+            for(let i = 0; i < qObject.drink.recipe.length; i++) {
+                let returnAmount = qObject.drink.recipe[i].amount;
+                let currentLoc = qObject.locations[i];
+                this.reservedShelf.bottles[currentLoc].amount += returnAmount;
+            }
+            return true;
+        } catch() {
+            return false;
+        }
     }
     
     // Function for removing the poured amount from the bottle in the correct location after the pour action
     // has been completed.
+    // rather simple but keeps the implementation more module.
     pourCompleted(location, amount) {
+        this.currentShelf[location].amount =- amount;
         return;
     }
     
     // A function for simply importing both bottleshelf and drinkDB at once. On a second thought completely useless? 
+    // Maybe for a "read/write default structure"
     importDB(drinkDB_file, bottleShelf_file) {
         return;
     }
     
     // A function for simply exporting both bottleshelf and drinkDB at once. On a second thought completely useless?  
+    // Maybe for a "read/write default structure."
     exportDB(drinkDB_file, bottleShelf_file) {
         return;
     }
     
     // A function that checks availability of the drinks. To be used by main program when adding a drink to the database. 
     checkDrinkAvailability(drinkName) {
+        // Create a variable for the drinks availability:
+        let isAvailable = true;
+        // Get the drinks recipe:
+        let recipe;
+        for(let i = 0; i < this.drinkDB.drinks.length; i++) {
+            if(this.drinkDB.drinks[i].name == drinkName) {
+
+            }
+        }
+
         return;
     }
     
