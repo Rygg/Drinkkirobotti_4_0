b0d72fa57e219105dbcc18800c51fe964c8a80a4
Ville Lepistö <ville.lepisto@hotmail.com>
2016-04-05 22:00:58 +0300

    Jatkettu robottirajapintaa.

--- Backend/robot.js
+++ Backend/robot.js
@@ -5,28 +5,31 @@
 // if problems -> sudo npm install --unsafe-perm serialport
 const SerialPort = require("serialport");
 
+let timeout_occurred = false;
 // Serial Port configuration:
 const port = '/dev/ttyUSB0';
-// Create the serialPort variable:
-let serialPort = new SerialPort.SerialPort(port, {
-    baudrate: 9600
-});
 
-class Robot {
+class Robot { 
     // Construct the robot class:
     constructor() {
-        // Set the flags for monitoring process from outside the class.
-        this.busy = false;
+        // Link the serialport to the robot-class as a member variable to export it outside:
+        this.serialPort = new SerialPort.SerialPort(port, {
+        baudrate: 9600
+        });
+        // Set the flags for monitoring the robot process.
+        this.communicating = false;
+        this.working = false;
+        this.lastCommand = 'none';
 
         // Open the Serial Port connection
-        serialPort.open(function(err) {
+        this.serialPort.open(function(err) {
             if(err) {
                 throw err; // Port failed to open:
             }
-            // Open a constant listening to the port: 
+            // Open a test listening and writing to the port (TESTING PURPOSES) 
             else {
                 console.log("SerialPort connection to controller opened.");
-                serialPort.write("Ma ajan koko yon", function(err,res) {
+                this.serialPort.write("Ma ajan koko yon", function(err,res) {
                     if(err) {
                         throw err;
                     } else {
@@ -34,49 +37,35 @@ class Robot {
                     }
 
                 });
-                serialPort.on('data', function(data) {    
+                this.serialPort.on('data', function(data) {    
                     console.log("Tuli dataa: ");
                     console.log(data);
                     i++;
-                    serialPort.close();
+                    this.serialPort.close();
                 });
             }
         })
     }    
-    // Other functions for writing to the serial port for robot control.
     
-    // Grabbing a bottle from the shelf - (Boolean):
-    //
+    // Other functions for writing to the serial port for robot control - False is unable to comply.
+    
+    // Grabbing a bottle from the shelf:
     grabBottle(location, type) {
-        // Check if the robot is busy.
-        if(this.busy) {
-            console.log("Robot is busy, grabBottle-command not registered.");
+        if(!ableToMove) {
             return false;
         }
-        // Doublecheck if the parameters are defined and proper. 
-        if(typeof(location) != 'number' || location < 0 || location > 11  || typeof(type) != 'string') {
-            console.log("Error with parameters. grabBottle-command not registered.");
+        // Check possible last commands:
+        if(this.lastCommand == 'pourDrinks' || this.lastCommand == 'grabBottle') {
+            console.log("Unable to execute command: The robot is already holding a bottle");
             return false;
         }
         // Robot should be able to perform the task:
-        this.busy = 1;
+        this.communicating = true;
         let command = 'grabBottle(' + location + ',' + type + ')';
-        // Set timeout calculator for the write operation (5 seconds for testing purposes):
-        let timeout = setTimeout(function() {
-            console.log("Program timed out.");
-        }, 5000);
         
-        serialPort.write(command, function(err,result) {
-            clearTimeout(timeout);
-            try {
-                responseHandler(err, result);
-            } catch(error) {
-                console.log("Error occurred while writing to serial: " + error);
-            }
-        });
-        // 
-        
-        
+        // Write the command to the serialport.
+        this.lastCommand = 'grabBottle';
+        writeSerial(command);
     }
     
     // Pouring a drink from the grabbed bottle - (Boolean):
@@ -99,19 +88,69 @@ class Robot {
         
     } 
     
+    
+    
 };
 
-function responseHandler(err, result) {
-    
+// Function to check if the robot is busy.
+function ableToMove() {
+    // Check if the robot is busy.
+    if(this.communicating || this.working) {
+        console.log("Unable to execute command: Robot is busy.");
+        return false;
+    }
+    // Doublecheck if the parameters are defined and proper. 
+    if(typeof(location) != 'number' || location < 0 || location > 11  || typeof(type) != 'string') {
+        console.log("Error with parameters. grabBottle-command not registered.");
+        return false;
+    }
+    return true;
+}
+
+// Function to write to write a command to the serial port.
+function writeSerial(command) {
+    // Set timeout calculator for the write operation (5 seconds for testing purposes):
+    let timeout = setTimeout(function() {
+        timeout_occurred = true;
+        console.log("Program timed out.");
+    }, 5000);
+    // Write the command to the serial connection. 
+    this.serialPort.write(command, function(err,result) {
+        if(timeout_occurred) {
+            return;
+        }
+        clearTimeout(timeout);
+        try {
+            responseHandler(err, result);
+        } catch(error) {
+            console.log("Error occurred while writing to serial: " + error);
+        }
+    });
+}
+
+// Function to be called if writing to port succeeds.
+function responseHandler(err, result, success) {
     if(err) {
         throw err;
     }
     console.log("Wrote to serial, waiting for response:");
-    serialPort.on('data', function(){
-    
+    this.serialPort.on('data', function(err, data){
+        if(err) {
+            throw err;
+        }
+        if(data == 'working') {
+            this.working = true;
+            this.communicating = false;
+            console.log("Robot is working");
+        } else {
+            this.working = false;
+            this.communicating = false;
+            console.log("Error: Robot couldn't execute the command.");
+        }
     })
       
 }
+
 
 let i = 0;
 
@@ -121,3 +160,7 @@ Rob.grabBottle(5,'Bombay');
 
 
 //serialPort.close();
+
+
+// Export serialport connection and robot module
+module.exports = Robot;
