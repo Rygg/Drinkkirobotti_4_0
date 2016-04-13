5c712e21a9047b7238e01ea8e54c1e8ae708b7ae
Ville Lepistö <ville.lepisto@student.tut.fi>
2016-04-13 12:54:27 +0000

    Muokattu server.js ottamaan vastaan drinkNameja

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ io.on('connection', function(socket){
 		console.log('new order!');
 		ID = ID+1;
                 console.log("Tilattiin "+drinkName+", tarjoillaan GT.");
-                order = { "ID": ID, "drinkName": "GT", "orderer":ordererName }
+                order = { "ID": ID, "drinkName": drinkName, "orderer":ordererName }
                 console.log(order);
                 JSONI = JSON.stringify(order);
 		backend.processOrder(JSONI);
