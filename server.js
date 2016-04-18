5317cac0ecc706b8f5bf0e54e6c2ed73cf410176
Ville Lepistö <ville.lepisto@hotmail.com>
2016-04-18 15:06:25 +0300

    Jau

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ function getFreeShelfPlace() {
 	let bottles = backend.database.currentShelf.getBottles();
 	let reserved_places = [];
 	// Hae varatut paikat ja lisää ne reserved_places listaan.
-	for (i = 0; i <= bottles.length; i++){
+	for (let i = 0; i <= bottles.length; i++){
 		bottle_locations = backend.database.currentShelf.findBottleLocations(bottles[i]);
 		for (j = 0; j <= bottle_locations.length; j++){
 			reserved_places.push(bottle_locations[j]);
